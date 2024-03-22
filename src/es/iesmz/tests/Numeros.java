a76749febaa2d1db1f9fd8dcc6e1f67fa9a4f43f
Miguel Bayona <mbayona03@gmail.com>
2024-03-22 10:29:00 +0100

    Método soniguales implementado

--- src/es/iesmz/tests/Numeros.java
+++ src/es/iesmz/tests/Numeros.java
@@ -1,4 +1,13 @@
 package es.iesmz.tests;
 
 public class Numeros {
+    public static int soniguales(int a, int b, int c) {
+        if (a == b && a == c) {
+            return 0;
+        } else if (a == b || a == c || b == c) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
 }
