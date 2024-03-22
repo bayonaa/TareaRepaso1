1af8210f62c9af07491f3dcde9667dc497f00041
Miguel Bayona <mbayona03@gmail.com>
2024-03-22 10:31:23 +0100

    Implementación de los imports, el BeforeEach y los dos primeros tests

--- src/es/iesmz/tests/NumerosTest.java
+++ src/es/iesmz/tests/NumerosTest.java
@@ -1,4 +1,22 @@
 package es.iesmz.tests;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NumerosTest {
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("EXECUTE PROVA");
+    }
+
+    @Test // todos diferentes
+    public void test1() {
+        assertEquals(2, Numeros.soniguales(4, 55, 7));
+    }
+
+    @Test // todos diferentes
+    public void test2() {
+        assertEquals(2, Numeros.soniguales(14, 23, 27));
+    }
 }
